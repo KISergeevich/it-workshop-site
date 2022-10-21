67b0c816fdf52b80c5f3e7b11919b062c8109258
Kalinin Ivan <indimion57@gmail.com>
2022-10-21 22:57:43 +0300

    new script and new maket

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+/*
+let readMore = document.querySelector('.readmore__button-arrow');
+let brends = document.querySelector('.brends');
+
+readMore.onclick = function() {
+    brends.classList.toggle('brends--expanded');
+
+    if (brends.classList.contains('brends--expanded')) {
+        readMore.textContent = 'Скрыть';
+    } else {
+        readMore.textContent = 'Читать далее';
+    }
+}
+*/
