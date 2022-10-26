fdca0c964aab5f0873e7f16e3019fe6d99e0b71e
Kalinin Ivan <indimion57@gmail.com>
2022-10-27 00:34:38 +0300

    new correct js + some buttons

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 
 let readMore = document.querySelector('.readmore__button-arrow');
+
 let brends = document.querySelector('.brends');
 
 readMore.onclick = function() {
     brends.classList.toggle('brends--expanded');
+    readMore.classList.toggle('readmore__button-arrow--reverse');
 
     if (brends.classList.contains('brends--expanded')) {
         readMore.textContent = 'Скрыть';
