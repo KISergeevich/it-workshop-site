3b6fe83f0538437078e011ac60e143bf99002ae8
Kalinin Ivan <indimion57@gmail.com>
2022-10-27 01:05:15 +0300

    перенос кода скрипта в отдельный документ

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-
-
-
 let readMore = document.querySelector('.readmore__button-arrow');
 
 let brends = document.querySelector('.brends');
 
-readMore.onclick = function() {
+readMore.onclick = function () {
     brends.classList.toggle('brends--expanded');
     readMore.classList.toggle('readmore__button-arrow--reverse');
 
@@ -16,3 +13,33 @@ readMore.onclick = function() {
     }
 }
 
+let mySwiper;
+function breakpointChecker(event) {
+    if (event.matches === true) {
+        if (mySwiper !== undefined) {
+            mySwiper.destroy(true, true);
+        }
+    } else if (event.matches === false) {
+        enableSwiper();
+    }
+};
+
+function enableSwiper() {
+    mySwiper = new Swiper('.swiper', {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        slidesPerView: 'auto',
+        slideClass: 'swiper-slide',
+        centeredSlides: false,
+        spaceBetween: 16,
+    });
+};
+const breakpoint = window.matchMedia('(min-width:767px)');
+
+breakpoint.addEventListener('change', breakpointChecker);
+// breakpointChecker(breakpoint); грязный нетипизированный хак :)
+if (breakpoint.matches === false) {
+    enableSwiper();
+} 
