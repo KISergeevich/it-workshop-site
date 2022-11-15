c817af4299cbb1984c135c8965cfab4c17a00b4b
Kalinin Ivan <indimion57@gmail.com>
2022-11-15 22:16:46 +0300

    подключил вебпак, изменил положение файлов

---
+++ src/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 
+                {
+                    loader: 'css-loader',
+                    options: {
+                        esModule: false
+                    },
+                },
+            ],
+          },
+        ],
+      },
+};
