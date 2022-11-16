1a33b7246eb567c7a7a79b9432fa3e1575906ff0
Kalinin Ivan <indimion57@gmail.com>
2022-11-16 21:31:12 +0300

    обновление для fonts in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -17,7 +17,15 @@ module.exports = {
             template: './index.html'
         }),
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                  from: path.resolve(__dirname, 'src/assets/IMG'),
+                  to:   path.resolve(__dirname, 'dist/images')
+                }
+              ]
+            })
     ],
     module: {
         rules: [
@@ -36,6 +44,10 @@ module.exports = {
                 test: /\.(png|jpg|svg|gif)$/,
                 use: ['file-loader']
             },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            }
         ],
     },
 };
