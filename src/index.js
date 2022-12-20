80f3568a57a361a09a263f18bf1313011a78d89c
Kalinin Ivan <indimion57@gmail.com>
2022-12-20 17:06:50 +0300

    щепотка отпимизации из маленьких функций

--- src/index.js
+++ src/index.js
@@ -81,40 +81,55 @@ if (breakpoint.matches === false) {
     priceSwiper = enableSwiper('price');
 } 
 
-function blurOn(opener) {
+
+function addBlur() {
+    const blur = document.querySelector('.blur');
     blur.classList.add('open-modal');
+}
+function removeBlur() {
+    const blur = document.querySelector('.blur');
+    blur.classList.remove('open-modal');
+}
+function addModal(element) {
+    if (element !== undefined) {
+        element.classList.add('open-modal');
+    }
+}
+function removeModal(element) {
+    if (element !== undefined) {
+        element.classList.remove('open-modal');
+    }
+}
+
+function blurOn(closeModal) { //closeModal - функция, которая будет вызвана на клике по блюру, что она делает и за что отвечает не дело функции этой.
+    const blur = document.querySelector('.blur');
+    addBlur();
     blur.addEventListener('click', function () {
-        opener.classList.remove('open-modal');
-        blur.classList.remove('open-modal');
+        removeBlur();
+        closeModal();
     });
 }
-function openModal(button, addModal, removeModal) {
+function openModal(button, modal, deleteModal) {
     button.addEventListener('click', function () {
-        addModal.classList.add('open-modal');
-        if (removeModal !== undefined) {
-            removeModal.classList.remove('open-modal');
-        }
-        blurOn(addModal);
+        addModal(modal);
+        removeModal(deleteModal);
+        blurOn(function() {
+            removeModal(modal)
+        });
     });
 }
 function closeModal(button, modal, menu) {
     button.addEventListener('click', function () {
-        modal.classList.remove('open-modal');
-        blur.classList.remove('open-modal');
-        if (menu !== undefined) {
-            if (menu.classList.contains('open-modal')) {
-                blur.classList.add('open-modal');
-            }
+        removeModal(modal);
+        if (menu === undefined || !menu.classList.contains('open-modal')) {
+            removeBlur();
         }
     });
 }
 
-
 const burgerMenu = document.querySelector('.burger-menu');
 const call = document.querySelector('.request-call');
 const feedback = document.querySelector('.feedback');
-
-const blur = document.querySelector('.blur');
 
 const burgerButton =  document.querySelector('.button-red__circle--burger-menu');
 
